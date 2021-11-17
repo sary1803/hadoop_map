3660f8f0e49a6dfbf21eeb1416c362e8d1b9e836
sary1803 <76999764+sary1803@users.noreply.github.com>
2021-11-17 20:29:12 +0100

    Update AppDriver.java

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -13,17 +13,17 @@ public class AppDriver {
                     "A map/reduce program that counts the words in the input files.");
 
             programDriver.addClass("kindtrees", KindTrees.class,
-                    "A map/reduce program that returns the distinct tree species (and the number of trees for each one) in the Remarkable Trees of Paris dataset.");
+                    "A map/reduce program that counts the different species in  tree.csv");
             programDriver.addClass("District", District.class,
-                    "A map/reduce program that returns the distinct districts with trees in the Remarkable Trees of Paris dataset.");
+                    "A map/reduce program that returns the distinct districts with trees  and the number of occurences in  tree.csv.");
 
             programDriver.addClass("species", Species.class,
-                    "A map/reduce program that returns the distinct tree species in the Remarkable Trees of Paris dataset.");
+                    "A map/reduce program that returns the distinct tree species in  tree.csv.");
             programDriver.addClass("maxHeight", MaxHeight.class,
-                    "A map/reduce program that returns the highest height of trees per species in the Remarkable Trees of Paris dataset.");
+                    "A map/reduce program that returns the highest height of trees per species in  tree.csv.");
 
             programDriver.addClass("SortHeight", SortHeight.class,
-                    "A map/reduce program that returns all the trees in the Remarkable Trees of Paris dataset, sorted by height.");
+                    "A map/reduce program that returns all the trees in tree.csv, sorted by height.");
 
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
